b37aff688a9a4929afab9b142c54956cf627dd1e
Taro Sato <taross@Tarossbook.local>
2017-04-11 11:35:12 +0900

    [add]Project files

--- www/laravel/database/factories/ModelFactory.php
+++ www/laravel/database/factories/ModelFactory.php
@@ -12,13 +12,10 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\User::class, function (Faker\Generator $faker) {
-    static $password;
 
+$factory->define(App\Task::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'is_done' => mt_rand(0, 1),
     ];
 });
