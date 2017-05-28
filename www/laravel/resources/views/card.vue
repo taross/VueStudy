b45fd7e20aa799457c97cb599d7c2734bb949c83
Taro Sato <taross@Tarossbook.local>
2017-05-28 19:57:13 +0900

    [add] メッセージカード素体.vueファイル追加

---
+++ www/laravel/resources/views/card.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="example">{{ msg }}</div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return {
+                msg : "Hello World!"
+            }
+        }
+    }
+</script>
+
+<style>
+    .example{
+        color: #0d3625;
+    }
+</style>
